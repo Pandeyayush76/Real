25a8153c1cdae32d41f01d0ebe82abd65b2df435
pande <pande@LAPTOP-7QPC96OE>
2022-07-15 12:33:58 +0530

    Done changes..

---
+++ src/test/java/com/crm/uk/tatamotors/CampaignTest.java
@@ -0,0 +1,20 @@
+package com.crm.uk.tatamotors;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class CampaignTest {
+ @Test(groups="smoke")
+ public void campaign()
+ {
+	 System.out.println("camapign created successfuly");
+ }
+ 
+ @Test(groups={"regression","smoke"})
+ public void contact()
+ {
+	 System.out.println("Contact failed successfully");
+ }
+
+}
