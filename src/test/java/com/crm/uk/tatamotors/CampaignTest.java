9de28cc20fd21b54fd8f93248425b553d28c9d7e
pande <pande@LAPTOP-7QPC96OE>
2022-07-15 17:26:25 +0530

    Nikhil do some changes in Campaign.

--- src/test/java/com/crm/uk/tatamotors/CampaignTest.java
+++ src/test/java/com/crm/uk/tatamotors/CampaignTest.java
@@ -1,8 +1,6 @@
 package com.crm.uk.tatamotors;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
-import org.testng.annotations.Test;
 
 public class CampaignTest {
  @Test(groups="smoke")
